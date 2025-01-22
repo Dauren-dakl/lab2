9dd5733df22e5b909d69951257985cd494ec1a70
Dauren-dakl <d_shaldarbek@kbtu.kz>
2025-01-22 16:26:26 +0500

    Python List

---
+++ lists/list/c.py
@@ -0,0 +1,8 @@
+list1 = ["abc", 34, True, 40, "male"]
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+
+thislist = list(("apple", "banana", "cherry")) # list- тик жакша  ашады
+print(thislist)
